0042ae0c04b622db3f96b7db235192737b4e4733
allisonrobbins <allisonrobbins@gwu.edu>
2018-12-11 23:49:55 -0500

    akdljsalkfjdsl

--- kernels.c
+++ kernels.c
@@ -34,14 +34,14 @@ static __inline__ unsigned long long rdtsc(void)
 /* end of definitions to set up measurement utilities */
 
 
-/* 
- * Please fill in the following team struct 
+/*
+ * Please fill in the following team struct
  */
 team_t team = {
-    "joe",              /* Team name */
+    "Allison",              /* Team name */
 
-    "Harry Q. Bovik",     /* First member full name */
-    "bovik@nowhere.edu",  /* First member email address */
+    "Allison Robbins",     /* First member full name */
+    "allisonrobbins@gwu.edu",  /* First member email address */
 
     "",                   /* Second member full name (leave blank if none) */
     ""                    /* Second member email addr (leave blank if none) */
@@ -75,21 +75,21 @@ long int getUserTime()
 	return (p->ru_utime.tv_sec * 1000000 + p->ru_utime.tv_usec);
 }
 
-/* 
- * naive_rotate - The naive baseline version of rotate 
+/*
+ * naive_rotate - The naive baseline version of rotate
  */
  /* The parameters, pointers, rusage_time, rdtsc_time, and cpu_time_used are used to measure performance and return values to caller. */
  /* You should not change the code that uses these parameters and variables. */
- 
+
 char naive_rotate_descr[] = "naive_rotate: Naive baseline implementation";
-void naive_rotate(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned long long *rdtsc_time) 
+void naive_rotate(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned long long *rdtsc_time)
 {
 	int i, j;
 	/* the variables below are used for performance measurement and not for computing the results of the algorithm */
 	long int rusage_start_time, rusage_end_time = 0;
 	unsigned long long rdtsc_start_time, rdtsc_end_time = 0;
 	/* call system functions to start measuring performance. you should not bother to change these. */
-	
+
 	rusage_start_time = getUserTime();
 	rdtsc_start_time = rdtsc();
 
@@ -111,7 +111,7 @@ void naive_rotate(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned lo
  /* The parameters, pointers, rusage_time, rdtsc_time, and cpu_time_used are used to measure performance and return values to caller. */
  /* You should not change the code that uses these parameters and variables. */
 char my_rotate_descr[] = "my_rotate: Naive baseline implementation";
-void my_rotate(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned long long *rdtsc_time) 
+void my_rotate(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned long long *rdtsc_time)
 {
 	int i, j;
 		/* the variables below are used for performance measurement and not for computing the results of the algorithm */
@@ -123,11 +123,26 @@ void my_rotate(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned long 
 
 /* ANY CHANGES ARE MADE HERE */
 /* below are the main computations for your implementation of the rotate. Any changes in implementation will go here or the other functions it may call */
-	for (j = 0; j < dim; j++)
-		for (i = 0; i < dim; i++)
-			dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
-
-
+  for (i = 0; i < dim; i+=4){
+    for (j = 0; j < dim; j+=4){
+      dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];//i,j
+      dst[RIDX(dim-1-j, i+1, dim)] = src[RIDX(i+1, j, dim)];//i+1,j
+      dst[RIDX(dim-1-j, i+2, dim)] = src[RIDX(i+2, j, dim)];//i+2,j
+      dst[RIDX(dim-1-j, i+3, dim)] = src[RIDX(i+3, j, dim)];//i+3,j
+      dst[RIDX(dim-1-(j+1), i, dim)] = src[RIDX(i, j+1, dim)];//i,j+1
+      dst[RIDX(dim-1-(j+1), i+1, dim)] = src[RIDX(i+1, j+1, dim)];//i+1,j+1
+      dst[RIDX(dim-1-(j+1), i+2, dim)] = src[RIDX(i+2, j+1, dim)];//i+2,j+1
+      dst[RIDX(dim-1-(j+1), i+3, dim)] = src[RIDX(i+3, j+1, dim)];//i+3,j+1
+      dst[RIDX(dim-1-(j+2), i, dim)] = src[RIDX(i, j+2, dim)];//i,j+2
+      dst[RIDX(dim-1-(j+2), i+1, dim)] = src[RIDX(i+1, j+2, dim)];//i+1,j+2
+      dst[RIDX(dim-1-(j+2), i+2, dim)] = src[RIDX(i+2, j+2, dim)];//i+2,j+2
+      dst[RIDX(dim-1-(j+2), i+3, dim)] = src[RIDX(i+3, j+2, dim)];//i+3,j+2
+      dst[RIDX(dim-1-(j+3), i, dim)] = src[RIDX(i, j+3, dim)];//i,j+3
+      dst[RIDX(dim-1-(j+3), i+1, dim)] = src[RIDX(i+1, j+3, dim)];//i+1,j+3
+      dst[RIDX(dim-1-(j+3), i+2, dim)] = src[RIDX(i+2, j+3, dim)];//i+2,j+3
+      dst[RIDX(dim-1-(j+3), i+3, dim)] = src[RIDX(i+3, j+3, dim)];//i+3,j+3
+    }
+  }
 /* end of computation for rotate function. any changes you make should be made above this line. */
 /* END OF CHANGES in this function */
 
@@ -161,26 +176,26 @@ typedef struct {
 } pixel_sum;
 
 /* Compute min and max of two integers, respectively */
-static int minimum(int a, int b) 
+static int minimum(int a, int b)
 { return (a < b ? a : b); }
-static int maximum(int a, int b) 
+static int maximum(int a, int b)
 { return (a > b ? a : b); }
 
-/* 
- * initialize_pixel_sum - Initializes all fields of sum to 0 
+/*
+ * initialize_pixel_sum - Initializes all fields of sum to 0
  */
-static void initialize_pixel_sum(pixel_sum *sum) 
+static void initialize_pixel_sum(pixel_sum *sum)
 {
 	sum->red = sum->green = sum->blue = 0;
 	sum->num = 0;
 	return;
 }
 
-/* 
- * accumulate_sum - Accumulates field values of p in corresponding 
- * fields of sum 
+/*
+ * accumulate_sum - Accumulates field values of p in corresponding
+ * fields of sum
  */
-static void accumulate_sum(pixel_sum *sum, pixel p) 
+static void accumulate_sum(pixel_sum *sum, pixel p)
 {
 	sum->red += (int) p.red;
 	sum->green += (int) p.green;
@@ -189,10 +204,10 @@ static void accumulate_sum(pixel_sum *sum, pixel p)
 	return;
 }
 
-/* 
- * assign_sum_to_pixel - Computes averaged pixel value in current_pixel 
+/*
+ * assign_sum_to_pixel - Computes averaged pixel value in current_pixel
  */
-static void assign_sum_to_pixel(pixel *current_pixel, pixel_sum sum) 
+static void assign_sum_to_pixel(pixel *current_pixel, pixel_sum sum)
 {
 	current_pixel->red = (unsigned short) (sum.red/sum.num);
 	current_pixel->green = (unsigned short) (sum.green/sum.num);
@@ -200,18 +215,18 @@ static void assign_sum_to_pixel(pixel *current_pixel, pixel_sum sum)
 	return;
 }
 
-/* 
- * avg - Returns averaged pixel value at (i,j) 
+/*
+ * avg - Returns averaged pixel value at (i,j)
  */
-static pixel avg(int dim, int i, int j, pixel *src) 
+static pixel avg(int dim, int i, int j, pixel *src)
 {
 	int ii, jj;
 	pixel_sum sum;
 	pixel current_pixel;
 
 	initialize_pixel_sum(&sum);
-	for(ii = maximum(i-1, 0); ii <= minimum(i+1, dim-1); ii++) 
-		for(jj = maximum(j-1, 0); jj <= minimum(j+1, dim-1); jj++) 
+	for(ii = maximum(i-1, 0); ii <= minimum(i+1, dim-1); ii++)
+		for(jj = maximum(j-1, 0); jj <= minimum(j+1, dim-1); jj++)
 			accumulate_sum(&sum, src[RIDX(ii, jj, dim)]);
 
 	assign_sum_to_pixel(&current_pixel, sum);
@@ -223,16 +238,16 @@ static pixel avg(int dim, int i, int j, pixel *src)
  ******************************************************/
 
 /*
- * naive_smooth - The naive baseline version of smooth 
+ * naive_smooth - The naive baseline version of smooth
  */
   /* The parameters, pointers, rusage_time, rdtsc_time, and cpu_time_used are used to measure performance and return values to caller. */
  /* You should not change the code that uses these parameters and variables. */
- 
+
 char naive_smooth_descr[] = "naive_smooth: Naive baseline implementation";
-void naive_smooth(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned long long *rdtsc_time) 
+void naive_smooth(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned long long *rdtsc_time)
 {
 	int i, j;
-	
+
 	/* the variables below are used for performance measurement and not for computing the results of the algorithm */
 	long int rusage_start_time, rusage_end_time = 0;
         unsigned long long rdtsc_start_time, rdtsc_end_time = 0;
@@ -256,19 +271,19 @@ void naive_smooth(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned lo
 
  /* The parameters, pointers, rusage_time, rdtsc_time, and cpu_time_used are used to measure performance and return values to caller. */
  /* You should not change the code that uses these parameters and variables. */
- 
+
 char my_smooth_descr[] = "my_smooth: Naive baseline implementation";
-void my_smooth(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned long long *rdtsc_time) 
+void my_smooth(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned long long *rdtsc_time)
 {
 	int i, j;
-	
+
 	/* the variables below are used for performance measurement and not for computing the results of the algorithm */
 	long int rusage_start_time, rusage_end_time = 0;
         unsigned long long rdtsc_start_time, rdtsc_end_time = 0;
 	/* call system functions to start measuring performance. you should not bother to change these. */
         rusage_start_time = getUserTime();
 	rdtsc_start_time = rdtsc();
-	
+
 /* ANY CHANGES TO BE MADE SHOULD BE BELOW HERE */
 /* below are the main computations for your implementation of the smooth function. Any changes in implementation will go here or the other functiosn it calls */
 
